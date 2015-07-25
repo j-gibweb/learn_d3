c01634b5f974dafae7e23cf6e1099837b136d89c
James Gibson Weber <j.gibweb@gmail.com>
2015-07-25 13:57:33 -0700

    make circles representing investment firms who's diameters reflect the amount of their investment

--- app/js/lib/d3/investorStatsExoplanets.js
+++ app/js/lib/d3/investorStatsExoplanets.js
@@ -10,27 +10,34 @@ export function drawExoplanets(data) {
     .attr('height', height)
     
 
-  var investorNames = Object.keys(data);
-
-  var investorDataTuplesArray = investorNames.map((investor) => {
-    return [investor, data[investor].reduce((accum, record) => { return accum += record.amount}, 0)]
+  var investorDataTuplesArray = Object.keys(data).map((investorName) => {
+    return [investorName, data[investorName].reduce((accumulator, recordOfInvestment) => { 
+      return accumulator += recordOfInvestment.amount
+    }, 0)]
   })
-  // console.log(investorDataTuplesArray)
-
 
   var scale = d3.scale.linear()
-    .domain([10, d3.max(investorDataTuplesArray)[1]])
-    .range([10, 30])
+    .domain([1, d3.max(investorDataTuplesArray)[1]])
+    .range([2, 3])
     .nice();
-
-  // console.log(scale(7500000))
 
   svg.selectAll('circle')
     .data(investorDataTuplesArray)
     .enter().append("circle")
-    .attr("cy", function(d, i) { console.log(`${d}`); return 60; })
-    .attr("cx", function(d, i) { console.log(`${d[1]}`); return i * 100 + 30; })
-    .attr("r", function(d) { console.log(Math.sqrt(d[1])); return scale(d[1]); });
+    .attr("cy", function(d, i) { 
+      return 60 * (Math.floor((i) / 6) + 1); 
+    })
+    .attr("cx", function(d, i) { 
+      var x = i - (Math.floor(i / 6) * 6) // start over every 6 indexes
+      return ((x * 100) + 50); 
+    })
+    .attr("r", 1)
+    .transition()
+    .duration(800)
+    .attr("r", function(d) { 
+      // console.log(Math.sqrt(d[1])); 
+      return scale(d[1]); 
+    });
 
 
   // var circle = d3.selectAll("circle");
